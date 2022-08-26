5e3847b12eedbfb7907b04351e6217368ae23433
omar2004712 <o.bahutar@gmail.com>
2022-08-26 23:14:41 +0400

    feat(search): added a change handler for the search box
    when the user inputs a search term a requestis sent to the wiki api to request for data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,35 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Search() {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    if (!term) {
+      return;
+    }
+
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
