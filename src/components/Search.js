333629492bf799bb57a445b35bca581da75762c0
omar2004712 <o.bahutar@gmail.com>
2022-08-26 22:08:58 +0400

    build: built a search component

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+
+export default function Search() {
+  const [term, setTerm] = useState('');
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
