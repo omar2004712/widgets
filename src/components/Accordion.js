8e1bffdfb98409d9fc367c70fc6ea443374ce669
omar2004712 <o.bahutar@gmail.com>
2022-08-26 20:50:35 +0400

    feat: added expand one slot at a time

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,32 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 
 export default function Accordion({ items }) {
-  const renderedItems = items.map((item) => (
-    <div key={item.title}>
-      <div className="title active">
-        <i className="dropdown icon" />
-        {item.title}
-      </div>
-      <div className="content active">
-        <p>{item.content}</p>
-      </div>
-    </div>
-  ));
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  function onTitleClick(index) {
+    if (activeIndex === index) {
+      return setActiveIndex(null);
+    }
+
+    return setActiveIndex(index);
+  }
+
+  const renderedItems = items.map((item, idx) => {
+    const active = idx === activeIndex ? 'active' : '';
+
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(idx)}>
+          <i className="dropdown icon" />
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
   return <div className="ui styled accordion">{renderedItems}</div>;
 }
