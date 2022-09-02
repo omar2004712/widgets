ff8368abc3c8f1ce3b21e928c856f3c595420120
omar2004712 <o.bahutair@gmail.com>
2022-09-02 22:23:18 +0400

    build: built the dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,63 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Dropdown({ options, selected, onSelectedChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+
+        setIsOpen(false);
+      },
+      // eslint-disable-next-line comma-dangle
+      { capture: true }
+    );
+  }, []);
+
+  const renderOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      // to hide the selected option from the list of options
+      return null;
+    }
+
+    return (
+      <div
+        onClick={() => {
+          onSelectedChange(option);
+        }}
+        key={option.value}
+        className="item"
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select an option</label>
+        <div
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          className={`ui selection dropdown ${isOpen ? 'visible active' : ''}`}
+        >
+          <i className="dropdown icon" />
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${isOpen ? 'visible transition' : ''}`}>
+            {renderOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
