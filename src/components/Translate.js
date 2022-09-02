e9f859cafc54574c866224026ce42489254e1761
omar2004712 <o.bahutair@gmail.com>
2022-09-02 23:56:22 +0400

    build: built the request function to translate text using google translate api

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -18,14 +21,25 @@ const options = [
 
 export default function Translate() {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
+
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         label="select a language"
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text} />
     </div>
   );
 }
